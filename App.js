9cf97c3c5e18b6304d56ea8b29ce413d7b553e37
Prerna2008 <72653361+Prerna2008@users.noreply.github.com>
2022-02-22 19:02:25 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import MainScreen from './MainScreen';
+import DetailsScreen from './Details';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack'
+
+export default function App() {
+  return (
+    <AppContainer/>
+  );
+}
+
+const appStackNavigator = createStackNavigator(
+  {
+    Home:{
+      screen:MainScreen,
+      navigationOptions:{headerShown:false}
+    },
+    Details:{
+      screen:DetailsScreen
+    }
+  },
+  {
+    initialRouteName:"Home"
+  }
+)
+
+const AppContainer = createAppContainer(appStackNavigator);
